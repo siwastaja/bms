cec2f673ee06ea2a392c4a96abca93be57887e4b
Antti Alhonen <antti.alhonen@tut.fi>
2014-09-24 13:30:32 +0300

    Channelfifo: Replaced status byte with standard comm error message w/ addr 253

--- channelfifo/channelfifo.c
+++ channelfifo/channelfifo.c
@@ -18,55 +18,45 @@
 // Packet reveiced
 // INT/CLOCK outputs high.
 // Master sets data read = 1, disables all interrupts, is ready to read data,
-//            keeps data read high until receives clock pulse 
+//            keeps data read high until receives clock pulse
 // Clock out starts at 1. Every falling edge denotes a new bit.
-// First 8 bits is channelfifo status.
-// Then 32 bits of data.
+// 32 bits of data.
 // If a new packet is received during this, clocking of the data ceases temporarily. Master must wait.
 
-
-// Status packet
-// bit 0: RX error, do not read 32 further bytes
-// bit 1: Data overrun (packet received when FIFO = full)
-// bit 2: Fifo almost full (<=2 empty slots)
 
 #define STAT_RX_ERR      1
 #define STAT_OVERRUN     2
 #define STAT_ALMOST_FULL 4
 
-typedef struct
-{
-	uint8_t status;
-	data_t data;
-} element_t;
-
-typedef union
-{
-	element_t element;
-	uint8_t[5] block;
-} element_union;
-
 #define FIFO_SLOTS 8
 
 uint8_t status;
-element_union fifo[FIFO_SLOTS];
+data_t fifo[FIFO_SLOTS];
 
 uint8_t fifo_wr_idx = 0;
-uint8_t out_idx = 0; // 0..7 = status byte, 8 = data
 uint8_t fifo_rd_idx = 0; // 0 .. FIFO_SLOTS-1
-uint8_t fifo_rd_byte_idx = 0; // 0 .. 5
-uint8_t fifo_rd_bit_idx = 0; // 0 .. 8
+uint8_t fifo_rd_bit_idx = 0; // 0 ..31
 
+uint8_t overrun_cnt = 0;
 
 // Function takes some time; is used right after packet is received;
 // No new packet is expected during the push.
-void fifo_push(element_t* element)
+// New packets may be pushed during the pull, but nothing is pulled
+// during the push.
+void fifo_push(data_t* element)
 {
-	for(uint8_t i = 5; i >= 0; i--)
-		fifo[fifo_wr_idx].block[i] = element->block[i];
+	if(((fifo_wr_idx < FIFO_SLOTS-1) && (fifo_wr_idx+1 == fifo_rd_idx)) ||
+	   ((fifo_wr_idx == FIFO_SLOTS-1) && (0 == fifo_rd_idx)))
+	{
+		overrun_cnt++;
+		return;
+	}
+
 	fifo_wr_idx++;
 	if(fifo_wr_idx >= FIFO_SLOTS)
 		fifo_wr_idx = 0;
+
+	fifo[fifo_wr_idx].abcd = element->abcd;
 }
 
 #define DATA_1 sbi(PORTB, 1);
@@ -74,63 +64,46 @@ void fifo_push(element_t* element)
 #define CLK_1  sbi(PORTB, 2);
 #define CLK_0  cbi(PORTB, 2);
 
+#define DATA_RD (PINB&1)
 
 // Fifo pull pulls one bit at a time, as it can be interrupted by
 // a new packet being received.
-void fifo_pull_bit()
+// return 1 when more bits available, 0 when packet is done
+// Leaves CLK at 0 after the last bit.
+uint8_t fifo_pull_bit()
 {
-	if(out_idx < 8)
+	if(fifo[fifo_rd_idx].abcd&((uint32_t)(1<<fifo_rd_bit_idx)))
 	{
-		if(status&(1<<out_idx))
-			DATA_1;
-		else
-			DATA_0;
-		out_idx++;
-		CLK_0;
-		__asm__ __volatile__("nop");
-		__asm__ __volatile__("nop");
-		__asm__ __volatile__("nop");
-		__asm__ __volatile__("nop");
-		__asm__ __volatile__("nop");
+		DATA_1;
 	}
 	else
 	{
-		if(fifo[fifo_rd_idx].block[fifo_rd_byte_idx]&(1<<fifo_rd_bit_idx))
-			DATA_1;
-		else
-			DATA_0;
-		fifo_rd_bit_idx++;
-		CLK_0;
-		if(fifo_rd_bit_idx > 7)
-		{
-			fifo_rd_bit_idx = 0;
-			fifo_rd_byte_idx++;
-			if(fifo_rd_byte_idx > 5)
-			{
-				fifo_rd_byte_idx = 0;
-				fifo_rd_idx++;
-				out_idx = 0;
-			}
-			else
-			{
-				__asm__ __volatile__("nop");
-				__asm__ __volatile__("nop");
-				__asm__ __volatile__("nop");
-			}
-		}
-		else
-		{
-			__asm__ __volatile__("nop");
-			__asm__ __volatile__("nop");
-		}
+		DATA_0;
 	}
+
+	fifo_rd_bit_idx++;
+	CLK_0;
+	if(fifo_rd_bit_idx > 31)
+	{
+		fifo_rd_bit_idx = 0;
+		fifo_rd_idx++;
+		if(fifo_rd_idx >= FIFO_SLOTS)
+			fifo_rd_idx = 0;
+		return 0;
+	}
+	else
+	{
+		__asm__ __volatile__("nop");
+		__asm__ __volatile__("nop");
+		__asm__ __volatile__("nop");
+	}
+
 	CLK_1;
+	return 1;
 }
 
 int main()
 {
-	element_t rcv_data;
-
 	PRR = 0b00001111;
 
 	DDRB  = 0b00000110;
@@ -144,35 +117,23 @@ int main()
 
 	while(1)
 	{
-		uint8_t ret;
-	NO_RX_BACK_TO_SLEEP:
+		while(fifo_rd_idx != fifo_wr_idx)
+		{
+			CLK_1; // Interrupt main CPU
+			// Expect a few cycles of RD signal.
+			while(!DATA_RD);
+			while(!DATA_RD);
+			// Pull bits until a complete packet is done.
+			while(fifo_pull_bit());
+			_delay_ms(1);
+		}
+
 	  	sbi(GIFR,5); // Clear PCINT flag.
-	  	sei();
 		MCUCR = 0b00110000;
 		__asm__ __volatile__("sleep");
 		// NORMALLY, the program stays exactly here, until there is a falling
 		// edge on the input pin.
-		cli(); // 1 clk
 		MCUCR = 0b00000000;  // 1 clk
-
-		if(!receiving) // We woke up for nothing :(.
-			goto NO_RX_BACK_TO_SLEEP; 
-		// 4 clk
-
-		receiving = 0; // 2 clk
-
-		// Grand total from the edge: 15 us (measured).
-
-		ret = manchester_receive(&(rcv_data.data)); 
-
-		if(ret) // Error, but got at least the start bit ok.
-		{
-			// TODO: Indicate end-of-chain comm error
-		}
-		else  // Got the packet OK.
-		{
-			// TODO: Indicate received packet.
-		}
 
 	}
 
@@ -182,6 +143,7 @@ int main()
 
 ISR(PCINT0_vect)
 {
+	data_t rcv_data;
 	// It takes at least 6+4+12 clock cycles (2.75 us) to wake up
 	// and get here, so the signal should be stabilized at
 	// low level
@@ -201,18 +163,22 @@ ISR(PCINT0_vect)
 
 	// ~10.5us spent from the edge.
 
-	receiving = n_low_readings >= 6;
-	// ~11us
+	if(n_low_readings >= 6)
+	{
+		_delay_us(1.5);
+		uint8_t ret;
+		ret = manchester_receive(&rcv_data);
+		if(ret)
+		{
+			rcv_data.a = 253;
+			rcv_data.b = 0x03; // standard comm error
+			rcv_data.c = ret;
+			rcv_data.d = 0;
+		}
+		fifo_push(&rcv_data);
+	}
+  	sbi(GIFR,5); // Clear PCINT flag.
 
-	// In reality, 12.5 us (measured).
-	// ISR end overhead 15 clk = 1.875 us.
-	// We were either in the "sleep" instruction or in the shunting loop structure when
-	// the interrupt came, so go back where we were.
-} // Grand total from the edge 12 us.
-
-ISR(ADC_vect)
-{
-	// Function stub needed here to get back from ADC Noise Reduction Sleep when
-	// the conversion is done.
 }
+
 
